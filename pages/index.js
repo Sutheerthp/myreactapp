22c5bcf86a9161f89e2bb6607bc9b605ff27d1e1
sutheerthp <sutheerth.p9@gmail.com>
2023-05-24 13:02:19 +0530

    Add useState

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,24 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { useRouter } from 'next/router'
+import {useState} from 'react'
 
 const inter = Inter({ subsets: ['latin'] })
-
 export default function Home() {
   const router = useRouter();
   let sum = parseInt(router.query.a)+parseInt(router.query.b);
+  const [name, setName] = useState("");
   return  (
   <div>
+    <input
+       type="Text" 
+       onChange={(event) => setName(event.target.value)}
+       placeholder="name" />  
     <h1>{router.query.name
       ? `hello, ${router.query.name}`
       : "i dont know your name"}
     </h1>
-  <br/><p>{sum}</p>
+  <br/><p>{sum} ,, {name}</p>
   </div>
   )
   
